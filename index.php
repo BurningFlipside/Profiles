454413e03871ada5348ac47f0f4b1f11e1b535f9
Problem <pboyd04@gmail.com>
2018-10-03 21:21:07 +0000

    Remove redundant content div

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ $page = new ProfilesPage('Burning Flipside Profiles');
 $page->addWellKnownJS(JS_CHEET, false);
 
 $page->body .= '
-<div id="content">
     <h1>Welcome to the Burning Flipside Profile System</h1>
     <p>This system allows you to login to the new and improved Burning Flipside website systems.</p>
     <p>This system will contain all your private data seperately so as to help prevent unwanted display of your data on www.burningflipside.com.
@@ -19,8 +18,7 @@ if($page->user !== null)
         $page->addNotification('Your profile is not yet complete. Click <a href="profile.php" class="alert-link">here</a> to complete your profile.', $page::NOTIFICATION_WARNING);
     }
     $page->body .= '<h1>Need to reset your password?</h1>
-    <p>You can reset your password <a href="'.$page->resetUrl.'">here.</a></p>
-</div>';
+    <p>You can reset your password <a href="'.$page->resetUrl.'">here.</a></p>';
 }
 else
 {
@@ -28,8 +26,7 @@ else
     <h1>Need to register for the first time?</h1>
     <p>You can sign up for an account <a href="'.$page->registerUrl.'">here</a>.
     <h1>Forgot your username or password?</h1>
-    <p>You can lookup a forgotten username or reset your password <a href="'.$page->resetUrl.'">here.</a></p>
-</div>';
+    <p>You can lookup a forgotten username or reset your password <a href="'.$page->resetUrl.'">here.</a></p>';
 }
 
 $page->printPage();
